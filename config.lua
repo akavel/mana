ee38c3bf09bd239ca421d7839e6c2cafdc018c47
Mateusz Czapliński <czapkofan@gmail.com>
2019-12-26 20:55:39 +0100

    add git-bs oneliner

--- config.lua
+++ config.lua
@@ -5,21 +5,24 @@ nnn.handle(require 'nnn.winfs')
 -- TODO: nnn.handle('0install', require 'nnn.zeroinstall')
 -- TODO: nnn.handle('choco', require 'nnn.chocolatey')
 
+-- One-line scripts (~aliases)
 local oneliners = {
 	gd = "git diff";
 	gds = "git diff --staged";
-	gs = "git status";
 	gf = "git fetch --all";
-	glg = "git log --graph \"--pretty=format:%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr %%cd) %%C(cyan)%%an%%Creset\" \"--date=format:'%%d.%%m\" --abbrev-commit";
 	gl = "glg --all";
+	glg = "git log --graph \"--pretty=format:%%Cred%%h%%Creset -%%C(yellow)%%d%%Creset %%s %%Cgreen(%%cr %%cd) %%C(cyan)%%an%%Creset\" \"--date=format:'%%d.%%m\" --abbrev-commit";
+	gs = "git status";
+	-- Show git branches in ~ "most recently used" order
+	["git-bs"] = "git branch --sort=-committerdate";
 }
-
 -- Render oneliners to full files
 for name, text in pairs(oneliners) do
 	-- TODO: make this work correctly on Linux/Mac
 	nnn.wanted["c/bin/" .. name .. ".bat"] = "@" .. text .. " %*"
 end
 
+-- Execute --
 local winfs = require 'nnn.winfs'
 nnn.osmkdirp = winfs.mkdirp
 nnn.exec_with(function(path)
