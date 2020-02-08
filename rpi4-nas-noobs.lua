4f3c6102d98b818285bbeab6e2356a7cc348a0e2
Mateusz Czapliński <czapkofan@gmail.com>
2020-02-08 01:40:39 +0100

    config for RPi NAS; WIP

---
+++ rpi4-nas-noobs.lua
@@ -0,0 +1,28 @@
+local mana = assert(io.popen('mana', 'w'))
+mana:write [[
+com.akavel.mana.v1
+shadow c:\prog\shadow-rpi4-nas-noobs
+handle boot lua53 mana/winfs.lua 4a8bd31f-4939-11ea-96ee-4074e03cda01
+]]
+
+function want(path)
+  return function(contents)
+    mana:write("want " .. path .. "\n")
+    for line in contents:gmatch "[^\n]*" do
+      mana:write(" " .. line .. "\n")
+    end
+  end
+end
+
+-- Enable SSH server in NOOBS
+-- https://raspberrypi.stackexchange.com/a/67353
+want "boot/ssh" ""
+
+-- Original contents
+want "boot/cmdline.txt" [[
+dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=/dev/mmcblk0p7 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait quiet init=/usr/lib/raspi-config/init_resize.sh splash plymouth.ignore-serial-consoles sdhci.debug_quirks2=4
+]]
+
+mana:write [[
+affect
+]]
