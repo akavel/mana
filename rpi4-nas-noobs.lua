e3e6f26cd8cf9e9732df4de76c9cc70e72e701e5
Mateusz Czapliński <czapkofan@gmail.com>
2020-02-11 00:47:12 +0100

    flashed 2020-02-05-raspbian-buster-full.img from scratch

--- rpi4-nas-noobs.lua
+++ rpi4-nas-noobs.lua
@@ -2,7 +2,7 @@ local mana = assert(io.popen('mana', 'w'))
 mana:write [[
 com.akavel.mana.v1
 shadow c:\prog\shadow-rpi4-nas-noobs
-handle boot lua53 handler/winfs.lua 4a8bd31f-4939-11ea-96ee-4074e03cda01
+handle boot lua53 handler/winfs.lua 4a8bd31d-4939-11ea-96ee-4074e03cda01
 ]]
 
 function want(path)
@@ -18,10 +18,10 @@ end
 -- https://raspberrypi.stackexchange.com/a/67353
 want "boot/ssh" ""
 
--- Set static IP
--- https://raspberrypi.stackexchange.com/a/59661
+-- Original, for now.
+-- See also: https://raspberrypi.stackexchange.com/a/59661
 want "boot/cmdline.txt" [[
-dwc_otg.lpm_enable=0 console=serial0,115200 console=tty1 root=/dev/mmcblk0p7 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait ip=192.168.0.195 quiet init=/usr/lib/raspi-config/init_resize.sh splash plymouth.ignore-serial-consoles sdhci.debug_quirks2=4
+console=serial0,115200 console=tty1 root=PARTUUID=093bedcc-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait quiet init=/usr/lib/raspi-config/init_resize.sh splash plymouth.ignore-serial-consoles
 ]]
 
 -- WiFi config
