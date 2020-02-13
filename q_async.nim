33411ab7b88ddb01120ac27287d17a58ad6b9ef5
Mateusz Czapliński <czapkofan@gmail.com>
2020-02-14 00:45:07 +0100

    works!

--- q_async.nim
+++ q_async.nim
@@ -1,4 +1,5 @@
 # {.experimental: "codeReordering".}
+import os
 import asynctools/[asyncproc, asyncpipe]
 import asyncfutures
 import asyncdispatch
@@ -12,10 +13,10 @@ var perr = p.errorHandle
 # proc next() {.async.} =
 proc next() =
 
-  var bufo = newString(1)
+  var bufo = newString(64)
   var fo = pout.readInto(bufo[0].addr, bufo.len)
   # let fo = readInto(pout, bufo[0].addr, bufo.len)
-  var bufe = newString(1)
+  var bufe = newString(64)
   var fe = perr.readInto(bufe[0].addr, bufe.len)
   # var fx = p.waitForExit()
   # await (fo or fe)
@@ -23,6 +24,7 @@ proc next() =
   #   return (fo or fe)
   # await xx()
   while true:
+    # discard waitFor(fo)
     # echo "await"
     # await fe or fo or fx
     # await fe or fo
@@ -30,11 +32,15 @@ proc next() =
     # echo "got"
     if fo.finished:
       if fo.read > 0:
-        echo "O ", fo.read(), " ", bufo.substr(0, fo.read()-1)
+        # echo "O ", fo.read(), " ", bufo.substr(0, fo.read()-1)
+        stdout.writeLine("O " & $fo.read() & " " & bufo.substr(0, fo.read()-1 ))
+        stdout.flushFile()
       fo = pout.readInto(bufo[0].addr, bufo.len)
     if fe.finished:
       if fe.read > 0:
-        echo "E ", fe.read(), " ", bufe.substr(0, fe.read()-1)
+        # echo "E ", fe.read(), " ", bufe.substr(0, fe.read()-1)
+        stdout.writeLine("E " & $fe.read() & " " & bufe.substr(0, fe.read()-1 ))
+        stdout.flushFile()
       fe = perr.readInto(bufe[0].addr, bufe.len)
     # if fx.finished:
     #   echo "X ", fx.read()
