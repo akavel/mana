eeae37a6f6110072f8faab970b38e0e887d20856
Mateusz Czapliński <czapkofan@gmail.com>
2024-01-17 08:59:43 +0100

    main: add TODOs + git2 dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
+    // TODO: two-way compare: current git <-> results of handlers.query (use git-workspace)
+    // TODO: parse input
+    // TODO: 3-way compare: curr git <-> handlers.query results <-> parsed input
+    // TODO: https://github.com/akavel/drafts/blob/main/20231122-001-mana2.md
     println!("Hello, world!");
 }
