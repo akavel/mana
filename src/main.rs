8736fd56219cfe1133d788439b993a5a49b9879f
Mateusz Czapliński <czapkofan@gmail.com>
2024-01-18 00:02:27 +0100

    feat(main): extract 'shadow_dir', 'handlers', 'tree' from TOML

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Context;
+use anyhow::{bail, Context};
 
 fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
@@ -9,6 +9,35 @@ fn main() -> anyhow::Result<()> {
         .parse::<toml::Table>()
         .context("failed to parse stdin as TOML")?;
     // println!("PARSED: {toml:?}");
+
+    // Extract `shadow_dir` from toml
+    // TODO[LATER]: use serde instead to extract, maybe
+    let Some(shadow_dir) = toml.get("shadow_dir") else {
+        bail!("Missing 'shadow_dir' in stdin");
+    };
+    let toml::Value::String(shadow_dir) = shadow_dir else {
+        bail!("Expected 'shadow_dir' to be text, got: {shadow_dir:?}");
+    };
+    println!("SHAD: {shadow_dir:?}");
+
+    // Extract `handlers` from toml
+    // TODO[LATER]: use serde instead to extract, maybe
+    let Some(handlers) = toml.get("handlers") else {
+        bail!("Missing 'handlers' in stdin");
+    };
+    let toml::Value::Table(handlers) = handlers else {
+        bail!("Expected 'handlers' to be table, got: {handlers:?}");
+    };
+    println!("HANDL: {handlers:?}");
+
+    // Extract `tree` from toml
+    // TODO[LATER]: use serde instead to extract, maybe
+    let Some(tree) = toml.get("tree") else {
+        bail!("Missing 'tree' in stdin");
+    };
+    let toml::Value::Table(tree) = tree else {
+        bail!("Expected 'tree' to be table, got: {tree:?}");
+    };
 
     // TODO: two-way compare: current git <-> results of handlers.gather (use git-workspace)
     // TODO: 3-way compare: curr git <-> handlers.query results <-> parsed input
