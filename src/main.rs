cfd09d967349cd33078334fb05857d46194a4b8e
Mateusz Czapliński <czapkofan@gmail.com>
2024-10-10 20:46:15 +0200

    switch to remotefs-ssh

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use anyhow::{bail, Context, Result};
 use clap::Parser;
-use ssh2::Session;
+use remotefs::RemoteFs;
+use remotefs_ssh::{ScpFs, SshOpts};
 
-use std::net::TcpStream;
 use std::path::{Path, PathBuf};
 
 #[derive(Parser)]
@@ -19,21 +19,30 @@ fn main() -> Result<()> {
     let cli = Cli::parse();
 
     //FIXME: try connecting to rpi4
-    let tcp = TcpStream::connect((cli.host.as_str(), 22))?;
-    let mut sess = Session::new()?;
-    sess.set_tcp_stream(tcp);
-    sess.handshake()?;
-    sess.userauth_pubkey_file(&cli.user, None, &cli.key_path, None)?;
-
-    println!("auth? {}", sess.authenticated());
+    let mut client: ScpFs = SshOpts::new(&cli.host)
+        .username(&cli.user)
+        .key_storage(Box::new(SshKeyPath { path: cli.key_path }))
+        .into();
+    println!("auth? {:?}", client.connect());
 
     //FIXME: try reading home dir /home/pi
-    let mut h = crate::Handler { sess };
+    let mut h = crate::Handler { client };
     let f = Path::new("/home/pi/xssh-test");
     use crate::callee::Handler;
-    println!("detect? {f:?} = {:?}", h.detect(&f));
+    println!("detect? {f:?} = {:?}", h.detect(f));
 
     Ok(())
+}
+
+#[derive(Clone)]
+struct SshKeyPath {
+    path: PathBuf,
+}
+
+impl remotefs_ssh::SshKeyStorage for SshKeyPath {
+    fn resolve(&self, _host: &str, _username: &str) -> Option<PathBuf> {
+        Some(self.path.clone())
+    }
 }
 
 pub mod callee {
@@ -48,12 +57,11 @@ pub mod callee {
 }
 
 pub struct Handler {
-    sess: Session,
+    client: ScpFs,
 }
 
 impl callee::Handler for Handler {
     fn detect(&mut self, path: &Path) -> Result<bool> {
-        let (chan, stat) = self.sess.scp_recv(path)?;
-        Ok(stat.is_file())
+        Ok(self.client.exists(path)?)
     }
 }
