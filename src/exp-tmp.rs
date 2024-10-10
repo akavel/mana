dc15253917165a9cd0c5d6ff99c207c9a307e9f7
Mateusz Czapliński <czapkofan@gmail.com>
2024-10-10 22:24:05 +0200

    exp-tmp

---
+++ src/exp-tmp.rs
@@ -0,0 +1,20 @@
+use anyhow::{bail, Context, Result};
+use std::path::{Path, PathBuf};
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(env="XSSH_HOST")]
+    host: String,
+    #[arg(env="XSSH_USER")]
+    user: String,
+    #[arg(env="XSSH_KEY")]
+    key_path: String,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    //FIXME: try connecting to rpi4
+    //FIXME: try reading home dir /home/pi
+    Ok(())
+}
