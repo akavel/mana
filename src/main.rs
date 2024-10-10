0e318de7f411ff9e4be8f5c4bce6a21e797e0626
Mateusz Czapliński <czapkofan@gmail.com>
2024-10-10 19:33:56 +0200

    parse env args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{bail, Context, Result};
+use clap::Parser;
+
+use std::path::{Path, PathBuf};
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(env="XSSH_HOST")]
+    host: String,
+    #[arg(env="XSSH_USER")]
+    user: String,
+    #[arg(env="XSSH_KEY")]
+    key_path: String,
 }
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    //FIXME: try connecting to rpi4
+    //FIXME: try reading home dir /home/pi
+    Ok(())
+}
+
