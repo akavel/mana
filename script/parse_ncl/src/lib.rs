423b9dc6dec072cf6c34d1bdc724da026a604d71
Mateusz Czapliński <czapkofan@gmail.com>
2024-10-24 20:34:53 +0200

    refactor: extract pkg script/parse_ncl
    To aid in faster dev cycle in script/

---
+++ script/parse_ncl/src/lib.rs
@@ -0,0 +1,32 @@
+use anyhow::{bail, Context, Result};
+use std::path::PathBuf;
+use toml::macros::Deserialize;
+
+pub fn from_file(ncl_path: PathBuf) -> Result<toml::Table> {
+    let username = whoami::username();
+    let hostname = whoami::hostname();
+    let field_path_raw = format!("{username}@{hostname}");
+
+    use nickel_lang_core::{
+        error::report::ErrorFormat, eval::cache::lazy::CBNCache, identifier::LocIdent,
+        pretty::ident_quoted, program::Program as Prog,
+    };
+    let field_path = ident_quoted(&LocIdent::new(field_path_raw));
+    // println!("FIELD: {field_path:?}");
+    use std::io::stderr;
+    let mut prog = Prog::<CBNCache>::new_from_file(&ncl_path, stderr())?;
+    let res_field = prog.parse_field_path(field_path.clone());
+    let Ok(field) = res_field else {
+        prog.report(res_field.unwrap_err(), ErrorFormat::Text);
+        bail!("failed to parse {field_path:?} as Nickel path");
+    };
+    prog.field = field;
+    let res_term = prog.eval_full_for_export();
+    let Ok(term) = res_term else {
+        prog.report(res_term.unwrap_err(), ErrorFormat::Text);
+        bail!("script {ncl_path:?} failed");
+    };
+    let toml = toml::Table::deserialize(term).context("loading Nickel output to TOML")?;
+    Ok(toml)
+}
+
--- script/src/lib.rs
+++ script/src/lib.rs
@@ -1,8 +1,7 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{bail, Result};
 use log::debug;
 use std::collections::BTreeMap;
 use std::path::PathBuf;
-use toml::macros::Deserialize;
 
 #[derive(Debug)]
 pub struct Script {
@@ -16,30 +15,7 @@ pub type PathContentMap = BTreeMap<String, String>;
 
 impl Script {
     pub fn parse_ncl_file(ncl_path: PathBuf) -> Result<Self> {
-        let username = whoami::username();
-        let hostname = whoami::hostname();
-        let field_path_raw = format!("{username}@{hostname}");
-
-        use nickel_lang_core::{
-            error::report::ErrorFormat, eval::cache::lazy::CBNCache, identifier::LocIdent,
-            pretty::ident_quoted, program::Program as Prog,
-        };
-        let field_path = ident_quoted(&LocIdent::new(field_path_raw));
-        // println!("FIELD: {field_path:?}");
-        use std::io::stderr;
-        let mut prog = Prog::<CBNCache>::new_from_file(&ncl_path, stderr())?;
-        let res_field = prog.parse_field_path(field_path.clone());
-        let Ok(field) = res_field else {
-            prog.report(res_field.unwrap_err(), ErrorFormat::Text);
-            bail!("failed to parse {field_path:?} as Nickel path");
-        };
-        prog.field = field;
-        let res_term = prog.eval_full_for_export();
-        let Ok(term) = res_term else {
-            prog.report(res_term.unwrap_err(), ErrorFormat::Text);
-            bail!("script {ncl_path:?} failed");
-        };
-        let mut toml = toml::Table::deserialize(term).context("loading Nickel output to TOML")?;
+        let mut toml = parse_ncl::from_file(ncl_path)?;
         Self::parse_toml(&mut toml)
     }
 
